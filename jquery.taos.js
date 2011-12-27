832e3186ef16c7e810a5727ba781a36798611601
Ryan Van Etten <ryanve@gmail.com>
2011-12-27 13:50:52 -0500

    added @link

--- jquery.taos.js
+++ jquery.taos.js
@@ -2,6 +2,7 @@
  * TAOS (Toggle Areas Or Styles) jQuery Plugin
  * @author Ryan Van Etten/2011
  * @license MIT
+ * @link http://github.com/ryanve/jquery.taos
  * @version 0.1.0
  * @requires jQuery 1.7+
  */
